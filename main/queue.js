c9143a498ace8ae50f45de1a7d46e34a6cf145af
WenHsi <kevinwen2014@gmail.com>
2025-03-04 20:31:04 +0800

    queue.js correction

--- main/queue.js
+++ main/queue.js
@@ -1,85 +1,35 @@
-class Event {
-    constructor() {
-        this.types = {};
-        this.onceTypes = {};
-    }
-
-    addListener(name, cb) {
-        if (!this.types[name]) {
-            this.types[name] = [];
-        }
-        if (this.types[name].indexOf(cb) != -1) return;
-
-        this.types[name].push(cb);
-        return cb
-    }
-
-    once(name, cb) {
-        if (!this.onceTypes[name]) {
-            this.onceTypes[name] = [];
-        }
-        if (this.onceTypes[name].indexOf(cb) != -1) return;
-        this.onceTypes[name].push(cb);
-        return cb
-    }
-
-    emit(name, data) {
-        if (this.types[name]) {
-            for (const cb of this.types[name]) {
-                cb(data);
-            }
-        }
-        
-        if (this.onceTypes[name]) {
-            // 不按照監聽順序，隨機給不同使用者號碼。
-            const random = Math.floor(Math.random() * this.onceTypes[name].length);
-            const cb = this.onceTypes[name][random];
-            if (!cb) return;
-            cb(data);
-            this.onceTypes[name].splice(random, 1);
-        };
-    }
-
-    remove(name) {
-        if (!this.types[name]) return;
-        delete this.types[name];
-    }
-}
-
-const eventInst = new Event();
-
 class Queue {
     constructor() {
         this.queue = [];
-        this.counter = 1;
+        this.channel = [];
     }
-
     enqueue(data) {
         this.queue.push(data);
+        this._emit();
     }
-
     async dequeue() {
-        return this._checkAndReturnQueueValue();
-    }
-
-    async _checkAndReturnQueueValue() {
         if (!this.queue.length) {
-            const p = await new Promise((resolve) => {
-                eventInst.once('haveNewValue', (v) => {
-                    resolve(v);
-                })
-            })
-            return p;
+            const id = await this._waitEvent();
+            return id;
         }
         return this.queue.shift();
     }
+    async _emit() {
+        if (! this.channel.length) return;
 
-    increaseEverySecond() {
-        setInterval(() => {
-            this.enqueue(this.counter);
-            eventInst.emit('haveNewValue', this.counter);
-            this.counter++;
-        }, 1000)
+        const random = Math.floor(Math.random() * this.channel.length);
+        const request = this.channel[random];
+        request(this.queue.shift());
+        this.channel.splice(random, 1);
+    }
+    async _waitEvent() {
+        const result = await new Promise((resolve) => {
+            this.channel.push(_request);
+            function _request(id) {
+                resolve(id);
+            }
+        })
+        return result;
     }
 }
 
@@ -88,36 +38,49 @@ class Consumer {
         this.name = name;
         this.id = null;
     }
-
     async getId(id) {
         this.id = await id;
         return this;
     }
-}
-
-function createConsumer(count) {
-    const consumers = [];
-    for (let i = 1; i <= count; i++) {
-        consumers.push(new Consumer(i));
+    static createConsumer(count) {
+        const consumers = [];
+        for (let i = 1; i <= count; i++) {
+            consumers.push(new Consumer(i));
+        }
+        return consumers;
     }
-    return consumers;
 }
 
-async function getAllConsumerIdFromQueue(consumers, queueInst) {
-    const promises = [];
+let queueInst;
+class Id {
+    constructor() {
+        this.id = 1;
+    }
+    generateId() {
+        setInterval(() => {
+            queueInst.enqueue(this.id);
+            this.id++;
+        }, 1000)
+    }
+}
+
+async function getIdForAllConsumer(consumers) {
+    const p = [];
     for (const c of consumers) {
-        promises.push(c.getId(queueInst.dequeue()));
+        p.push(c.getId(queueInst.dequeue()));
     }
-    const result = await Promise.all(promises);
+    const result = await Promise.all(p);
     return result;
 }
 
 async function start() {
-    const queueInst = new Queue();
-    queueInst.increaseEverySecond();
-    const consumers = createConsumer(5);
-    const result = await getAllConsumerIdFromQueue(consumers, queueInst);
-    
+    const idInst = new Id();
+    queueInst = new Queue();
+
+    idInst.generateId();
+    const consumers = Consumer.createConsumer(5);
+    const result = await getIdForAllConsumer(consumers);
+
     console.log(result);
     return result;
 }
